4b21a7b9323f60e80a491ce6f477fda25d47b6fb
Ahmed Abdelwahab <ahmed.abdelwahab@prophecylabs.com>
2019-03-07 03:07:26 +0200

    Add .env output

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
+const fs = require('fs');
 const awsSDK = require('aws-sdk');
+
 const ssm = new awsSDK.SSM();
-ssm.getParametersByPath({Path: '/development', WithDecryption: true}, (err, data) => {
-	err && console.error(err);
-	!err && console.log(data.Parameters);
+
+const paramsPath = process.argv[2]
+// make it false by default
+const recursive = (process.argv[3] || '') === 'true'? true : false
+
+ssm.getParametersByPath({Path: paramsPath, WithDecryption: true, Recursive: recursive}, (err, data) => {
+	if (err) {
+		console.error(err);
+		process.exit(1);
+	} else {
+		const lines = data.Parameters.map(parameter => {
+			const varName = (/[^\/]+$/g).exec()[0];
+			return `${parameter.Name.replace(/^.*\/(.*)$/g, '$1')}='${parameter.Value}'`;
+		});
+		fs.writeFileSync('./.env', lines.join('\n'));
+	}
 });
