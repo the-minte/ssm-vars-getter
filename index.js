9c89608cb6d241069735aa628ff3d8e1a6f5fa53
Ahmed Abdelwahab <ahmed.abdelwahab@prophecylabs.com>
2019-03-07 13:08:52 +0200

    Fix typos. Account for the pagination imposed by the API

--- index.js
+++ index.js
@@ -3,19 +3,72 @@ const awsSDK = require('aws-sdk');
 
 const ssm = new awsSDK.SSM();
 
-const paramsPath = process.argv[2]
+const paramsPath = process.argv[2];
 /* make it false by default */
-const recursive = (process.argv[3] || '') === 'true'? true : false
+const recursive = (process.argv[3] || '') === 'true' ? true : false;
 
-ssm.getParametersByPath({Path: paramsPath, WithDecryption: true, Recursive: recursive}, (err, data) => {
-	if (err) {
-		console.error(err);
-		process.exit(1);
-	} else {
-		const lines = data.Parameters.map(parameter => {
-			const varName = (/[^\/]+$/g).exec()[0];
-			return `${parameter.Name.replace(/^.*\/(.*)$/g, '$1')}='${parameter.Value}'`;
-		});
-		fs.writeFileSync('./.env', lines.join('\n'));
-	}
-});
+function getParametersByPath({ path, recursive, token }) {
+  return new Promise((resolve, reject) => {
+    ssm.getParametersByPath(
+      {
+        Path: path,
+        WithDecryption: true,
+        Recursive: recursive,
+        NextToken: token,
+      },
+      (err, data) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(data);
+        }
+      }
+    );
+  });
+}
+
+async function getAllParamsByPath({path, recursive}) {
+  let token = undefined;
+  let allParameters = [];
+  do {
+    const {Parameters, NextToken} = await getParametersByPath({path, recursive, token});
+    allParameters = allParameters.concat(Parameters);
+    token = NextToken
+  } while(token);
+  return allParameters;
+}
+
+function convertParamsJsonToEnv(parameters = []) {
+  const lines = parameters.map(parameter => {
+    const varName = /[^\/]+$/g.exec(parameter.Name)[0];
+    return `${varName}='${parameter.Value}'`;
+  });
+  return lines;
+}
+
+function saveToEnvFile(envLines) {
+  fs.writeFileSync('./.env', envLines.join('\n'));
+}
+
+async function main({path, recursive}) {
+  const parameters = await getAllParamsByPath({path, recursive});
+  console.log(parameters.length);
+  const envLines = convertParamsJsonToEnv(parameters);
+  saveToEnvFile(envLines);
+}
+
+main({
+  path: paramsPath,
+  recursive
+})
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  })
+  .then(() => {
+    process.exit(0);
+  });
+
+  module.exports = {
+    getParametersByPath
+  };
